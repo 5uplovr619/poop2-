257dc04a438ade8c22635d5bd8cb75b51d138bfc
5uplovr619 <145370202+5uplovr619@users.noreply.github.com>
2023-11-20 09:42:20 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+document.getElementById("btn").addEventListener("click", placeorder);
+
+
+function placeorder() {
+    alert("bullet")
+}
+
+document.getElementById("btn2").addEventListener("click", idiom1);
+
+function idiom1(){
+alert("cats and dogs")
+}
+
+document.getElementById("btn3").addEventListener("click", idiom2);
+
+function idiom2(){
+alert("straw")
+}
+
+document.getElementById("btn4").addEventListener("click", idiom3);
+
+function idiom3(){
+alert("that again")
+}
+
+document.getElementById("btn5").addEventListener("click", idiom4);
+
+function idiom4(){
+alert("rocket science")
+}
+
+document.getElementById("btn6").addEventListener("click", idiom5);
+
+function idiom5(){
+alert("an arm and a leg")
+}
+
+document.getElementById("btn7").addEventListener("click", idiom6);
+
+function idiom6(){
+alert("chicken butt")
+}
+
+
+
